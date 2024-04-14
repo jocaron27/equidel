4e3846df7045fd72f9c1380584956bda7c6ce1d5
Jeanne <jcaron27@gmail.com>
2024-04-14 18:09:40 -0500

    Add text to contact page

--- src/components/main/contact/contact.js
+++ src/components/main/contact/contact.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Contact() {
     return (
         <div className="contact">
+            <p>Like what you see? Get in touch to learn more.</p>
             <p>Email: equidelart@gmail.com</p>
             <p>Phone: (301) 859-0721</p>
         </div>
