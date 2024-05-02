1ab93b4b98bab85c613c907921b19b38121f97f1
Jeanne <jcaron27@gmail.com>
2024-05-02 01:12:19 -0500

    Create initial gallery

--- src/components/main/portfolio/portfolio.js
+++ src/components/main/portfolio/portfolio.js
@@ -1,35 +1,39 @@
 import React, { useContext, useState } from 'react';
 import { DataContext } from '../../../context';
 import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import { Anchor, Box, Button, Card, FlexBox, Text } from '../../styled';
+import { Anchor, Box, FlexBox, Grid, Text } from '../../styled';
 import { PorfolioContainer } from './portfolioStyledComponents';
 import { Quote } from '../../custom/quote';
+import { GalleryItem } from '../galleryItem';
+import { theme } from '../../../theme';
+import { useMediaQuery } from '../../../hooks';
 
 function Portfolio() {
-    const [modalOpen, setModalOpen] = useState(false);
-    const { data } = useContext(DataContext);
-
-    const toggleModal = () => {
-        setModalOpen(!modalOpen);
-    };
+    const { data: gallery } = useContext(DataContext);
+    const { isDesktop } = useMediaQuery();
+    const portfolioPadding = isDesktop ? [0] : [0, 5];
+    const portfolioMargin = isDesktop ? [10, 0] : [0];
+    const gridGap = isDesktop ? theme.spacing(12) : 0;
+    const gridMargin = isDesktop ? [20, 0] : [0];
 
     return (
-        <FlexBox $center $p={[0, 5]}>
+        <FlexBox $center $m={portfolioMargin} $p={portfolioPadding}>
           <PorfolioContainer>
             <Box>
               <Text>Welcome to my studio and gallery. One of my defining principles is conveying a sense of awe through the timeless medium of oil painting, establishing an open dialogue between form and emotion in all aspects of my artwork. Look through my work to see this in action, and <Anchor href="/contact">get in touch</Anchor> for more information. Some of my paintings can be seen at <Anchor href="https://www.garrettarts.org"  target="_blank">the Gallery Shop</Anchor> in Oakland, MD.</Text>
             </Box>
 
-            <Modal open={modalOpen} onClose={toggleModal} center>
-              <Card>
-                <Box>
-                  {JSON.stringify(data)}
-                </Box>
-              </Card>
-            </Modal>
-
-            <Button onClick={toggleModal}>Gallery item</Button>
+            <Grid $itemSize={75} $gap={gridGap} $m={gridMargin} $center>
+              {gallery?.map((galleryItem) => (
+                <GalleryItem
+                  key={galleryItem.id + galleryItem.fileName}
+                  imageName={galleryItem.name}
+                  imageSize={galleryItem.size}
+                  imageMedium={galleryItem.medium}
+                  fileName={galleryItem.fileName}
+                />
+              ))}
+            </Grid>
 
             <Box $m={[5]}>
               <Quote author="Leonardo da Vinci" image="https://cdn.jsdelivr.net/gh/jocaron27/equidel/public/assets/paint.jpeg" imageWidth={550}>"The artist sees what others only catch a glimpse of." </Quote>
